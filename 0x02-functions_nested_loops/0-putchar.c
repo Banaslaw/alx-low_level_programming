d16c035a656e315a729f8f575e668170170680ef
banas <indribuze@gmail.com>
2022-06-07 04:39:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+int _putchar(char c);
 #include <unistd.h>
 
 /**
@@ -11,4 +11,18 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
