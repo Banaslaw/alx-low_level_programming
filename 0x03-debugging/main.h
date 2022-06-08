1b1a4e425cf914733947cf5b38276cd5ab21609c
banas <indribuze@gmail.com>
2022-06-08 10:06:41 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int positive_or_negative(int i)
+{	
+	if (i>0)
+	{
+		printf("is positive%d",1);
+	}
+	else if (i ==0)
+	{
+		printf("is zero%d",i);
+	}
+	else { printf("is negative %d",i);
+}
